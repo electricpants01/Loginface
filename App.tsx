087e2b3678d38a4cebd4d592a4ba5e2d58632da8
Christian Torrico Avila <christorricoavila@gmail.com>
2021-08-05 07:54:24 -0400

    add source code

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+interface Props {
+  
+}
+
+const App = (props: Props) => {
+  return (
+    <View>
+      <Text>Hola christian</Text>
+      
+    </View>
+  )
+
+
+}
+
+export default App
